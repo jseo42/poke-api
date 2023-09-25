c488c85f17ddf91a659b131859e62dd48d582a58
jseo42 <jayseo.js@gmail.com>
2023-09-25 06:27:04 -0500

    clean async

--- pokedata.js
+++ pokedata.js
@@ -1,52 +1,32 @@
-async function getSinnoh() {
-  const nameList = [];
-  const sinnohURL = 'https://pokeapi.co/api/v2/pokedex/5/'
+export default async function getSinnohData() {
+  const sinnohURL = 'https://pokeapi.co/api/v2/pokedex/5/';
   try {
-    const response = await fetch(sinnohURL, {mode: "cors"});
+    const response = await fetch(sinnohURL, { mode: 'cors' });
     const data = await response.json();
-    data.pokemon_entries.forEach(entry => {
-      nameList.push(entry.pokemon_species.name)
+    const pokemonList = data.pokemon_entries.map((entry) => entry.pokemon_species.name);
+    
+    const pokemonDataPromises = pokemonList.map(async (pokemon) => {
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, { mode: 'cors' });
+        const pokemonData = await response.json();
+        return {
+          name: pokemonData.name,
+          id: pokemonData.id,
+          image: pokemonData.sprites['front_default'],
+          imageShiny: pokemonData.sprites['front_shiny'],
+          pokemonType: pokemonData.types.map((type) => type.type.name).join(', '),
+        };
+      } catch (e) {
+        console.error('Error fetching data for', pokemon, e);
+        return null;
+      }
     });
+    
+    const pokemonData = await Promise.all(pokemonDataPromises);
+    console.log(pokemonData.filter(Boolean));
+    return pokemonData.filter(Boolean);
   } catch (e) {
-    console.log('Something went wrong');
-    console.log(e);
+    console.error('Something went wrong', e);
+    throw e;
   }
-  return nameList;
 }
-
-async function getPokemonData(name) {
-  const pokemonURL = `https://pokeapi.co/api/v2/pokemon/${name}`;
-  try {
-    const response = await fetch(pokemonURL, {mode:"cors"});
-    const pokemonData = await response.json();
-    const usableData = {
-      name: pokemonData.name,
-      id: pokemonData.id,
-      image: pokemonData.sprites['front_default'],
-      imageShiny: pokemonData.sprites['front_shiny'],
-      pokemonType: pokemonData.types.map((type) => type.type.name)
-    };
-    return usableData
-  } catch (e) {
-    console.log('Something went wrong', e.message);
-    throw(e);
-  }
-  
-}
-
-export async function getSinnohData() {
-  const pokemonList = await getSinnoh();
-  const pokemonData = [];
-  for (const pokemon of pokemonList) {
-    try {
-      let details = await getPokemonData(pokemon);
-      pokemonData.push(details);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  console.log(pokemonData);
-  return pokemonData;
-}
-
-export default getSinnohData; 
